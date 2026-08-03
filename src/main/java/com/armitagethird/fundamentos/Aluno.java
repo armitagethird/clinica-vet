cc5888f2ef76f8435923063c3685c39e4089f58e
armitagethird <romerosaraiva4@gmail.com>
2026-08-03 16:04:38 -0300

    refactor: aula 3 - encapsula Aluno com construtor, getter e setter validado

--- src/main/java/com/armitagethird/fundamentos/Aluno.java
+++ src/main/java/com/armitagethird/fundamentos/Aluno.java
@@ -1,5 +1,19 @@
 package com.armitagethird.fundamentos;
 
-public class Aluno{
-    String nome;
+public class Aluno {
+    private String nome;
+
+    public Aluno(String nome){
+        setNome(nome);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        if (nome == null || nome.isBlank()){
+    throw new IllegalArgumentException("Nome não pode ser vazio ou nulo");
+        }
+        this.nome = nome;
+    }
 }
