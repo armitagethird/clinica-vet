a455f434ed4dfc306e06c7815af7b1547eb38f18
armitagethird <romerosaraiva4@gmail.com>
2026-08-07 18:00:53 -0300

    feat: aula 4 - sobrescreve sacar na ContaPoupanca com taxa fixa

--- src/main/java/com/armitagethird/app/ContaPoupanca.java
+++ src/main/java/com/armitagethird/app/ContaPoupanca.java
@@ -4,6 +4,7 @@ import com.armitagethird.banco.ContaBancaria;
 
 public class ContaPoupanca extends ContaBancaria {
     private double taxaRendimento;
+    private static final double TAXA_SAQUE = 2.0;
     public ContaPoupanca(String titular, double saldo, double taxa){
         super(titular, saldo);
         this.taxaRendimento = taxa;
@@ -15,6 +16,12 @@ public class ContaPoupanca extends ContaBancaria {
     public void experimentoVisibilidade(){
         System.out.println(numeroConta);
 
+    }
+
+    @Override
+    public void sacar(double valor){
+        System.out.println(" [poupanca] cobrando taxa de " + TAXA_SAQUE);
+        super.sacar(valor + TAXA_SAQUE);
     }
 
 
